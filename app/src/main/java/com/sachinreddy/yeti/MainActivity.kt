6524769fe322fb5428e88394b1ad1f04ef99f04e
sachinreddy1 <reddysachin2014@gmail.com>
2022-02-12 12:46:34 -0500

    .

--- app/src/main/java/com/sachinreddy/yeti/MainActivity.kt
+++ app/src/main/java/com/sachinreddy/yeti/MainActivity.kt
@@ -27,7 +27,6 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
-
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
