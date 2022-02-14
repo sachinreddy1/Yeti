17b24433f2748b1e67df617f06c8e7b8521c3904
sachinreddy1 <reddysachin2014@gmail.com>
2022-02-13 21:46:29 -0500

    Added image upload. Need to add API upload.

--- app/src/main/java/com/sachinreddy/yeti/fragment/SecondFragment.kt
+++ app/src/main/java/com/sachinreddy/yeti/fragment/SecondFragment.kt
@@ -1,13 +1,18 @@
 package com.sachinreddy.yeti.fragment
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import com.sachinreddy.yeti.R
 import com.sachinreddy.yeti.databinding.FragmentSecondBinding
+import java.io.FileNotFoundException
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -15,19 +20,65 @@ import com.sachinreddy.yeti.databinding.FragmentSecondBinding
 class SecondFragment : Fragment() {
 
     private var _binding: FragmentSecondBinding? = null
+    private val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+    private var permissionGranted = false
+    private val REQUEST_CODE = 12345
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View {
-
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
-        return binding.root
 
+        binding.uploadActionButton.setOnClickListener {
+            checkPermissions()
+        }
+
+        return binding.root
+    }
+
+    private fun checkPermissions() {
+        context?.let {
+            if (ContextCompat.checkSelfPermission(it, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                permissionGranted = false
+
+                Intent().apply {
+                    type = "image/*"
+                    action = Intent.ACTION_GET_CONTENT
+                    startActivityForResult(Intent.createChooser(this, "Title"), REQUEST_CODE)
+                }
+            } else {
+                ActivityCompat.requestPermissions(requireActivity(), permissions, REQUEST_CODE)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            permissionGranted = true
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        data?.let {
+            try {
+                binding.uploadImage.setImageURI(it.data)
+                binding.uploadActionButton.text = getString(R.string.upload)
+            } catch (e: FileNotFoundException) {
+                e.printStackTrace()
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
     override fun onDestroyView() {
